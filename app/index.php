0161286af4ed1648c768b93f8cb323428acfc93d
LysunAleksandr <94159313+LysunAleksandr@users.noreply.github.com>
2021-11-28 15:40:08 +0500

    Update index.php
    + index.php

--- app/index.php
+++ app/index.php
@@ -8,11 +8,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>crt-symfony-2</title>
 </head>
 <body>
-    <a href="./Task1/Main.php">Task1</a>
-    <a href="./Task2/Main.php">Task2</a>
-    <a href="./Task3/Main.php">Task3</a>
+<h1>crt-symfony-2</h1>
+    <a href="./Task1/Main.php">Task1 <br /></a>
+    <a href="./Task2/Main.php">Task2 <br /></a>
+    <a href="./Task3/Main.php">Task3 <br /></a>
 </body>
 </html>
